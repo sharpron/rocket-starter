c6bc5f9e5d41df1a9349d13dcc7e3edd209d27d2
ron <1358612584@qq.com>
2021-10-20 17:31:58 +0800

    删除无效配置、添加测试

---
+++ src/test/java/pub/ron/admin/logging/LogTest.java
@@ -0,0 +1,42 @@
+package pub.ron.admin.logging;
+
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * 日志测试.
+ *
+ * @author ron 2021/10/20
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LogTest {
+
+  @Resource
+  private LogExample logExample;
+
+  /**
+   * 测试@Log注解.
+   */
+  @Test
+  public void testLogAnnotation() {
+    makeMockRequest();
+    logExample.causeVisitLog("参数内容");
+    logExample.causeErrorLog();
+  }
+
+  /**
+   * 创建一个虚拟请求.
+   */
+  private void makeMockRequest() {
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    request.setRemoteAddr("10.1.1.11");
+    request.addHeader(HttpHeaders.USER_AGENT, "Mock Http Request/1.0");
+    // 设置mock请求的ip地址
+    RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+  }
+}
