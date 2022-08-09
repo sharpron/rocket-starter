62b4f3270b05a4d2fd833caaab9dcdb071e2747a
ron <1358612584@qq.com>
2022-08-09 18:22:57 +0800

    增加字典项查询功能

--- src/main/java/pub/ron/admin/system/rest/DictItemRest.java
+++ src/main/java/pub/ron/admin/system/rest/DictItemRest.java
@@ -27,7 +27,9 @@ import pub.ron.admin.common.validator.Create;
 import pub.ron.admin.common.validator.Update;
 import pub.ron.admin.logging.Log;
 import pub.ron.admin.system.body.DictItemBody;
+import pub.ron.admin.system.domain.DictItem;
 import pub.ron.admin.system.dto.DictItemQuery;
+import pub.ron.admin.system.dto.DictItemSmallDto;
 import pub.ron.admin.system.service.DictItemService;
 import pub.ron.admin.system.service.mapper.DictItemMapper;
 
@@ -58,6 +60,19 @@ public class DictItemRest {
   @Log("查询字典项")
   public ResponseEntity<?> findDictionaries(Pageable pageable, DictItemQuery dictItemQuery) {
     return ResponseEntity.ok(dictItemService.findByPage(pageable, dictItemQuery));
+  }
+
+
+  /**
+   * 字典格式的数据.
+   *
+   * @param dictItemQuery the query criteria
+   * @return 资源
+   */
+  @GetMapping(params = "datatype=dict")
+  public ResponseEntity<List<DictItemSmallDto>> getDictItemsAsDict(DictItemQuery dictItemQuery) {
+    List<DictItem> dictItems = dictItemService.findAll(dictItemQuery);
+    return ResponseEntity.ok(dictItemMapper.mapSmalls(dictItems));
   }
 
 
--- src/main/java/pub/ron/admin/system/service/mapper/DictItemMapper.java
+++ src/main/java/pub/ron/admin/system/service/mapper/DictItemMapper.java
@@ -1,12 +1,16 @@
 package pub.ron.admin.system.service.mapper;
 
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import pub.ron.admin.system.body.DictItemBody;
 import pub.ron.admin.system.domain.DictItem;
+import pub.ron.admin.system.dto.DictItemSmallDto;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface DictItemMapper {
 
   DictItem mapDict(DictItemBody dictItemBody);
+
+  List<DictItemSmallDto> mapSmalls(List<DictItem> dictItems);
 }
