85e51ef05bdf532eea0e45b5e25b2bbf4ef6e30c
ron <1358612584@qq.com>
2022-08-10 12:19:35 +0800

    使用generateAllSetter插件替代mapstruct

--- src/main/java/pub/ron/admin/quartz/service/mapper/QuartzJobMapper.java
+++ src/main/java/pub/ron/admin/quartz/service/mapper/QuartzJobMapper.java
@@ -1,12 +1,28 @@
 package pub.ron.admin.quartz.service.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import org.springframework.stereotype.Component;
 import pub.ron.admin.quartz.body.QuartzJobBody;
 import pub.ron.admin.quartz.domain.QuartzJob;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface QuartzJobMapper {
+@Component
+public class QuartzJobMapper {
 
-  QuartzJob mapQuartzJob(QuartzJobBody quartzJobBody);
+
+  /**
+   * 转换数据.
+   *
+   * @param quartzJobBody quartzJobBody
+   * @return quartzJob
+   */
+  public QuartzJob mapQuartzJob(QuartzJobBody quartzJobBody) {
+    QuartzJob quartzJob = new QuartzJob();
+    quartzJob.setName(quartzJobBody.getName());
+    quartzJob.setRunnableBeanName(quartzJobBody.getRunnableBeanName());
+    quartzJob.setParams(quartzJobBody.getParams());
+    quartzJob.setCronExpression(quartzJobBody.getCronExpression());
+    quartzJob.setEnabled(quartzJobBody.isEnabled());
+    quartzJob.setDescription(quartzJobBody.getDescription());
+    quartzJob.setId(quartzJobBody.getId());
+    return quartzJob;
+  }
 }
