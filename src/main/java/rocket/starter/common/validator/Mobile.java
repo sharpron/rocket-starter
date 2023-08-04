aa82120393cda4e9f7a0b9fd313f5ea3530323ed
ron <1358612584@qq.com>
2023-08-04 22:02:23 +0800

    修改最新手机号格式验证

--- src/main/java/rocket/starter/common/validator/Mobile.java
+++ src/main/java/rocket/starter/common/validator/Mobile.java
@@ -17,7 +17,7 @@ import javax.validation.constraints.Pattern;
 @Target(ElementType.FIELD)
 @Constraint(validatedBy = {})
 @Pattern(regexp =
-    "^((13\\d)|(14[014-9])|(15[0-3,5-9])|(16[2567])|(17[0-8])|(18\\d)|(19[0-3,5-9]))\\\\d{8}$",
+    "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$",
     message = "手机号格式错误"
 )
 public @interface Mobile {
