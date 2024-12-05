231180b986d3f26197b4ea5173ea342a984e40ae
ron <1358612584@qq.com>
2024-12-05 17:12:54 +0800

    feat: base entity增加验证validator注解

--- src/main/java/rocket/starter/common/BaseEntity.java
+++ src/main/java/rocket/starter/common/BaseEntity.java
@@ -1,5 +1,6 @@
 package rocket.starter.common;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import javax.persistence.Column;
@@ -8,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +20,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import rocket.starter.common.validator.Create;
+import rocket.starter.common.validator.Update;
 
 /**
  * 实体的基类.
@@ -35,22 +40,28 @@ public abstract class BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @NotNull(groups = Update.class)
+  @Null(groups = Create.class)
   private Long id;
 
   @Column(nullable = false, updatable = false)
   @CreatedBy
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private String createBy;
 
   @Column(nullable = false, updatable = false)
   @CreatedDate
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private LocalDateTime createTime;
 
   @Column(nullable = false)
   @LastModifiedBy
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private String modifyBy;
 
   @Column(nullable = false)
   @LastModifiedDate
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private LocalDateTime modifyTime;
 
   public BaseEntity(Long id) {
@@ -76,5 +87,4 @@ public abstract class BaseEntity {
   public final int hashCode() {
     return Objects.hashCode(this.id);
   }
-
 }
