842d0424122e5bff537e55631881977a280c63e9
ron <1358612584@qq.com>
2024-12-18 20:42:57 +0800

    fix: 修复查询自己的菜单存在重复数据的问题

--- src/main/java/rocket/starter/system/repo/MenuRepo.java
+++ src/main/java/rocket/starter/system/repo/MenuRepo.java
@@ -20,7 +20,7 @@ public interface MenuRepo extends BaseRepo<Menu> {
    * @param userId userId
    * @return 多个菜单
    */
-  @Query(value = "select * from sys_menu m left join sys_role_menu rm on m.id = rm.menu_id"
+  @Query(value = "select distinct m.* from sys_menu m left join sys_role_menu rm on m.id = rm.menu_id"
       + " left join sys_user_role ur on rm.role_id = ur.role_id"
       + " where ur.user_id = ?1 order by m.order_no",
       nativeQuery = true)
